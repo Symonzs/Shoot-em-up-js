4785d5ab085df65cc04b9c3293a4e73448853e77
simon <simon.hayart.etu>
2024-02-22 20:33:50 +0100

    test velocity X

---
+++ src/test/velocityX.test.js
@@ -0,0 +1,25 @@
+import assert from 'node:assert/strict';
+import Character from './Character.js';
+import Joueur from '../Joueur.js';
+
+const image = new Image();
+image.src = "/images/monster.png";
+
+describe('Joueur.velocityX', () => {
+  it('should return 0 when distance is 0', () => {
+    const joueur = new Joueur(image, 5, 1, 0, 0);
+    assert.strictEqual(joueur.velocityX(0), 0);
+  });
+
+ /* it('should return positive speed for positive distance', () => {
+    const joueur = new Joueur(image, 5, 1, 0, 0);
+    assert(joueur.velocityX(20) > 0);
+  });
+
+  it('should return negative speed for negative distance', () => {
+    const joueur = new Joueur(image, 5, 1, 0, 0);
+    assert(joueur.velocityX(-20) < 0);
+  });*/
+
+  // Ajoutez d'autres tests selon vos besoins
+});
