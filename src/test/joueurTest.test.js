6108db44ad5365fd02cf7ff0abadf39fc040c5e4
Armand Sady <armand.sady.etu@univ-lille.fr>
2024-02-27 12:09:32 +0100

    refacto tests joueur

--- src/test/joueurTest.test.js
+++ src/test/joueurTest.test.js
@@ -1,30 +1,41 @@
 import Joueur from "../joueur.js";
 import LinearMissile from "../LinearMissile.js";
+import { getRenderValues } from "../GetInitialValues.js";
 import { describe, it } from 'node:test';
 import assert from 'node:assert/strict';
 
+const artificialImageValues = {
+    "path": "/images/gentil.png",
+    "width": 100,
+    "height": 100
+}
+
+const rendervalues = getRenderValues(artificialImageValues, 0, 0)
+
 describe('joueur', () => {
     it('should have the right amount of health', () => {
-        const artificialImage = {
-            "path": "path",
-            "width": 80,
-            "height": 80
-          }
-        const player = new Joueur(artificialImage, 5, 10, 0, 0);
-        const bullet = new LinearMissile(artificialImage, 5, 10, 0, 0);
+        const player = new Joueur(5, 10, rendervalues);
+        const bullet = new LinearMissile("/images/basicbullet.png", 10, 999, rendervalues);
         player.hit(bullet);
         assert.strictEqual(player.hp, 9);
     });
-
+    
     it('should move correctly',() => {
-        const artificialImage = {
-            "path": "path",
-            "width": 80,
-            "height": 80
-          }
-          const player = new Joueur(artificialImage, 5, 10, 100, 100);
-          player.move();
-            assert.strictEqual(player.x, 90);
-            assert.strictEqual(player.y, 90);
+      const player = new Joueur(5, 10, rendervalues);
+      player.latestCursorX = player.renderCoordinates.x + player.renderCoordinates.width/2;
+      player.latestCursorY = player.renderCoordinates.y + player.renderCoordinates.height/2;
+      console.log(player.latestCursorX);
+      console.log(player.latestCursorY);
+      player.move();
+      assert.strictEqual(player.renderCoordinates.x, 0);
+      assert.strictEqual(player.renderCoordinates.y, 0);
     });
+    /*
+    it('should shoot correctly',() => {
+          const player = new Joueur(artificialImage, 5, 10, 100, 100);
+          player.shoot();
+          assert.strictEqual(player.missileList.length, 1);
+          assert.strictEqual(player.missileList[0].x, player.renderCoordinates.x);
+    });
+    */
 });
