107a9c7c77f6431b94d35203b6cba97f42884e13
Armand SADY <armand.sady.etu@univ-lille.fr>
2024-03-11 15:05:28 +0100

    push new image plamabullet

--- src/UpperCurvedMissile.js
+++ src/UpperCurvedMissile.js
@@ -1,7 +1,7 @@
 import Missiles from "./Missiles.js";
 
 export default class UpperCurvedMissile extends Missiles {
-  constructor(image, speed,hp, renderCoordinates) {
+  constructor(image, speed, hp, renderCoordinates) {
     super(speed, hp, renderCoordinates);
     if (!image) {
       this.image = "/images/bullets/basicbullet.png";
@@ -9,13 +9,14 @@ export default class UpperCurvedMissile extends Missiles {
       this.image = image;
     }
     this.canBeHurt = false;
-    this.amplitude = 100;//variation amx de la courbe en hauteur
-    this.frequency = 0.005;//longueur de la courbe en largeur
+    this.amplitude = 10; //variation amx de la courbe en hauteur
+    this.frequency = 0.005; //longueur de la courbe en largeur
     this.initialY = renderCoordinates.y;
   }
 
   move() {
     this.renderCoordinates.x -= this.speed;
-    this.renderCoordinates.y = this.initialY - Math.sin(this.renderCoordinates.x * this.frequency) * this.amplitude;
-}
+    this.renderCoordinates.y -=
+      Math.sin(this.renderCoordinates.x * this.frequency) * this.amplitude;
+  }
 }
