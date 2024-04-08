f35ef14a7ffa7787e7576b7564a2f06b95e78dd2
Armand Sady <armand.sady.etu@univ-lille.fr>
2024-04-08 23:50:48 +0200

    reverted JoueurRender

--- client/src/JoueurRender.js
+++ client/src/JoueurRender.js
@@ -1,4 +1,3 @@
-import renderEntity from "./EntityRender.js";
 import renderMissile from "./MissileRender.js";
 
 const playerImage = new Image();
@@ -6,7 +5,6 @@ const HPBar = new Image();
 HPBar.src = "/images/HUD/hpbar.png";
 const HP = new Image();
 HP.src = "/images/HUD/hp.png";
-const bulletIcon = new Image();
 
 function renderMainHP(player, context) {
   if (!player) {
@@ -24,14 +22,9 @@ function renderMainHP(player, context) {
       startingY + borderHeight
     );
   }
-  if (player.weaponIndex === 1) {
-    bulletIcon.src = "/images/HUD/bulletIcon2.png";
-  } else {
-    bulletIcon.src = "/images/HUD/bulletIcon1.png";
-  }
-  context.drawImage(bulletIcon, HPBar.width + 10, startingY);
 }
 function renderSecondaryHP(player, context, secondaryX, secondaryY) {
+  //console.log(secondaryX + " " + secondaryY);
   context.drawImage(HPBar, secondaryX, secondaryY);
 }
 
@@ -53,7 +46,11 @@ function renderHitbox(player, context) {
   context.strokeStyle = "black";
 }
 
-function renderName(player, context) {
+export function renderPlayer(player, context, socketID, additionalY) {
+  const secondaryX = player.canvasWidth;
+  const secondaryY = player.canvasHeight;
+  if (player.health <= 0) return;
+  playerImage.src = player.renderCoordinates.path;
   context.textAlign = "center";
   context.fillStyle = "white";
   context.fillText(
@@ -62,44 +59,25 @@ function renderName(player, context) {
     player.renderCoordinates.y - 5
   );
   context.fillStyle = "black";
-}
-
-function drawPlayer(player, context, isClient) {
-  if (isClient) {
-    playerImage.src = player.renderCoordinates.path;
-  } else {
-    playerImage.src = "/images/ships/allyship2.png";
-  }
-  if (player.canBeTouched) {
-    context.drawImage(
-      playerImage,
-      player.renderCoordinates.x,
-      player.renderCoordinates.y
-    );
-  } else {
-    context.globalAlpha = 0.1;
-    context.drawImage(
-      playerImage,
-      player.renderCoordinates.x,
-      player.renderCoordinates.y
-    );
-    context.globalAlpha = 1;
-  }
-  if (isClient) {
-    renderMainHP(player, context);
-  }
-}
-
-export function renderPlayer(player, context, socketID, additionalY) {
-  if (player.health <= 0) return;
-  const secondaryX = player.canvasWidth;
-  const secondaryY = player.canvasHeight;
-  renderName(player, context);
-  const isClient = player.id === socketID;
-  drawPlayer(player, context, isClient);
+  context.drawImage(
+    playerImage,
+    player.renderCoordinates.x,
+    player.renderCoordinates.y
+  );
   if (player.missileList) {
     player.missileList.forEach((missile) => {
       renderMissile(missile, context);
     });
+  }
+  //renderHitbox(player, context);
+  if (player.id === socketID) {
+    renderMainHP(player, context);
+  } else {
+    renderSecondaryHP(
+      player,
+      context,
+      secondaryX,
+      secondaryY - additionalY * 10
+    );
   }
 }
